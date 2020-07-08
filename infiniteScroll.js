f12fcb2a7233231d83161abb27b59ab935a665f1
Vladislav <nahabavladislav@gmail.com>
2020-07-08 15:57:16 +0300

    Update, added cache, interval options

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -1,5 +1,5 @@
 class InfiniteScroll {
-
+    
     constructor(container, urlConfig = {
         url: null,
         params: null
@@ -11,6 +11,9 @@ class InfiniteScroll {
         loader: null,
         loadingHtml: null,
         autoTrigger: true,
+        cache: false,
+        intervalCheck: false,
+        intervalTime: 40,
         nextSelector: null,
         autoTriggerUntil: null,
         requestType: 'get'
@@ -23,6 +26,9 @@ class InfiniteScroll {
             loader: null,
             loadingHtml: null,
             autoTrigger: true,
+            cache: false,
+            intervalCheck: false,
+            intervalTime: 40,
             nextSelector: null,
             autoTriggerUntil: null,
             requestType: 'get'
@@ -35,14 +41,18 @@ class InfiniteScroll {
         this.__$appendTo = this.__settings.appendTo ? this.__$container.find(this.__settings.appendTo) : this.__$container;
         this.__urlConfig = urlConfig;
 
+        this.__urlType = this.__checkUrlType(this.__urlConfig.url) ? 'absolute' : 'relative';
+
         this.__isWindow = (this.__$container.css('overflow-y') === 'visible');
 
+        this.__reachedStatus = false;
         this.__loading = false;
         this.__done = false;
         this.__errors = [];
         this.__counter = 0;
         this.__callbacks = {
             'init': () => {},
+            'reached': () => {},
             'response': () => {},
             'error': () => {},
             'last': () => {}
@@ -51,6 +61,34 @@ class InfiniteScroll {
         this.__$window = $(window);
         this.__$scroll = this.__isWindow ? this.__$window : this.__$container;
         this.__$body = $('body');
+
+        this.__cache = [];
+        if (this.__settings.cache) {
+            this.clearCache = () => {
+                this.__cache = [];
+            }
+            this.getFromCache = (index) => {
+                if (index >= this.__cache.length || index < 0 || !Number.isInteger(index)) {
+                    throw new Error('InfiniteScroll method getFromCache error: invalid index');
+                }
+                const cachedElement = this.__cache[index];
+                this.__cache.splice(1, index);
+                return cachedElement;
+            }
+            this.saveInCache = (data) => {
+                this.__cache.push(data);
+            }
+            this.makeAjax = (urlConfig) => {
+                if (!this.__done) {
+                    this.__loading = true;
+                    this.__ajaxRequest(urlConfig);
+                }
+            }
+            this.append = (template) => {
+                this.__$appendTo.append(template);
+                this.__reachedStatus = false;
+            };
+        }
     }
 
     get urlConfig() {
@@ -62,34 +100,44 @@ class InfiniteScroll {
     }
 
     init() {
-        this.__bindEventListeners();
-        this.__callbacks['init']();
+        if (this.__settings.intervalCheck) {
+            this.__intervalCheckScrollPosition();
+        } else {
+            this.__bindEventListeners();
+        }
+        this.__callbacks['init'](this.urlConfig);
     }
 
     __contructorValuesCheck(container, urlConfig, settings) {
         if (!window.jQuery) {
-            throw new Error('Constructor error: jQuery is not loaded');
+            throw new Error('InfiniteScroll constructor error: jQuery is not loaded');
         }
         if (!container) {
-            throw new Error('Constructor error: container is not specified');
+            throw new Error('InfiniteScroll constructor error: container is not specified');
         }
         if (!urlConfig) {
-            throw new Error('Constructor error: urlConfig is not specified');
+            throw new Error('InfiniteScroll constructor error: urlConfig is not specified');
         }
         if (!urlConfig.url) {
-            throw new Error('Constructor error: urlConfig.url is not specified');
+            throw new Error('InfiniteScroll constructor error: urlConfig.url is not specified');
         }
         if (!Number.isInteger(settings.scrollThreshold)) {
-            throw new Error('Constructor error: scrollThreshold must be a integer');
+            throw new Error('InfiniteScroll constructor error: scrollThreshold must be a integer');
         }
         if (!settings.autoTrigger && !settings.nextSelector) {
-            throw new Error('Constructor error: AutoTrigger set to false, but nextSelector is not defined');
+            throw new Error('InfiniteScroll constructor error: AutoTrigger set to false, but nextSelector is not defined');
+        }
+        if (settings.intervalCheck && !Number.isInteger(settings.intervalTime)) {
+            throw new Error('InfiniteScroll constructor error: intervalTime must be a integer');
+        }
+        if (settings.intervalCheck && settings.intervalTime < 0) {
+            throw new Error('InfiniteScroll constructor error: intervalTime can\'t be less than 0');
         }
         if (settings.autoTriggerUntil !== null && !Number.isInteger(settings.autoTriggerUntil)) {
-            throw new Error('Constructor error: autoTriggerUntil must be a integer');
+            throw new Error('InfiniteScroll constructor error: autoTriggerUntil must be a integer');
         }
         if (settings.autoTriggerUntil !== null && settings.autoTriggerUntil < 0) {
-            throw new Error('Constructor error: autoTriggerUntil can\'t be less than 0');
+            throw new Error('InfiniteScroll constructor error: autoTriggerUntil can\'t be less than 0');
         }
     }
 
@@ -133,10 +181,9 @@ class InfiniteScroll {
 
     __getSafeParams(url, params) {
         let urlObject = null;
-        const urlType = this.__checkUrlType(this.urlConfig.url) ? 'absolute' : 'relative';
-        if (urlType === 'absolute') {
+        if (this.__urlType === 'absolute') {
             urlObject = new URL(url);
-        } else if (urlType === 'relative') {
+        } else if (this.__urlType === 'relative') {
             urlObject = new URL(url, document.location.protocol + '//' + document.location.host + '/');
         } 
 
@@ -148,16 +195,36 @@ class InfiniteScroll {
         return urlObject.toString();
     }
 
-    __ajaxRequest() {
+    __clone(obj) {
+        if (obj === null || typeof (obj) !== 'object' || 'isActiveClone' in obj)
+            return obj;
+    
+        if (obj instanceof Date)
+            var temp = new obj.constructor();
+        else
+            var temp = obj.constructor();
+    
+        for (var key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                obj['isActiveClone'] = null;
+                temp[key] = this.__clone(obj[key]);
+                delete obj['isActiveClone'];
+            }
+        }
+        return temp;
+    }
+
+    __ajaxRequest(urlConfig) {
+        const deepClone = this.__clone(urlConfig);
         this.__showLoader();
         if (this.__settings.requestType.toLowerCase() === 'get') {
             $.ajax({
-                url: this.urlConfig.params
-                ? this.__getSafeParams(this.urlConfig.url, this.urlConfig.params)
-                : this.urlConfig.url,
+                url: urlConfig.params
+                ? this.__getSafeParams(urlConfig.url, urlConfig.params)
+                : urlConfig.url,
                 type: 'GET',
-                success: (data, status) => { 
-                    this.__successResponse(data, status);
+                success: (data, status) => {
+                    this.__successResponse(data, deepClone, status);
                 },
                 error: (error) => {
                     this.__errorResponse(error);
@@ -165,11 +232,11 @@ class InfiniteScroll {
             });
         } else if (this.__settings.requestType.toLowerCase() === 'post') {
             $.ajax({
-                url: this.urlConfig.url,
+                url: urlConfig.url,
                 type: 'POST',
-                data: this.urlConfig.params,
-                success: (data, status) => { 
-                    this.__successResponse(data, status);
+                data: urlConfig.params,
+                success: (data, status) => {
+                    this.__successResponse(data, deepClone, status);
                 },
                 error: (error) => {
                     this.__errorResponse(error);
@@ -178,11 +245,11 @@ class InfiniteScroll {
         }
     }
 
-    __successResponse(data) {
+    __successResponse(data, urlConfig, status) {
         this.__counter++;
         this.__loading = false;
         this.__hideLoader();
-        this.__responseHandler(data);
+        this.__responseHandler(data, urlConfig);
     }
 
     __errorResponse(error) {
@@ -194,7 +261,7 @@ class InfiniteScroll {
         const workContinue = () => {
             this.__done = false;
             this.__loading = true;
-            this.__ajaxRequest();
+            this.__ajaxRequest(this.__urlConfig);
         };
         this.__callbacks['error'](error, workContinue, this.urlConfig, this.__getInfo());
     }
@@ -220,28 +287,45 @@ class InfiniteScroll {
     }
 
     __getInfo() {
-        return {
+        let obj = {
             counter: this.__counter,
             loading: this.__loading,
             done: this.__done,
             errors: this.__errors,
             isWindow: this.__isWindow,
+            urlType: this.__urlType,
+            scrollFromTop: this.__$window.scrollTop(),
             settings: {
                 requestType: this.__settings.requestType,
                 scrollThreshold: this.__settings.scrollThreshold,
                 autoTrigger: this.__settings.autoTrigger,
                 autoTriggerUntil: this.__settings.autoTriggerUntil
             }
+        };
+        if (this.__settings.cache) {
+            obj.cache = this.__settings.cache;
+            obj.cachedAmount = this.__cache.length;
+            obj.reached = this.__reached() || this.__reachedStatus;
         }
+        if (this.__settings.intervalCheck) {
+            obj.intervalCheck = this.__settings.intervalCheck;
+            obj.intervalTime = this.__settings.intervalTime;
+        }
+        return obj;
     }
 
-    __responseHandler(data) {
+    __responseHandler(data, urlConfig) {
         const parsedData = this.__jsonObjectHandler(data);
         if (parsedData) {
             const append = (template) => {
                 this.__$appendTo.append(template);
+                this.__reachedStatus = false;
             };
-            this.__callbacks['response'](parsedData, append, this.urlConfig, this.__getInfo());
+            if (this.__settings.cache) {
+                this.__callbacks['response'](parsedData, append, urlConfig, this.__getInfo());
+            } else {
+                this.__callbacks['response'](parsedData, append, this.urlConfig, this.__getInfo());
+            }
         } else if (!parsedData || ((typeof parsedData === 'object') && parsedData.hasOwnProperty('done') && parsedData.done)) {
             this.__done = true;
             this.__renderLastHtmlTemplate();
@@ -260,14 +344,41 @@ class InfiniteScroll {
         this.__callbacks[event] = callback;
     }
 
+    __reached() {
+        const windowHeight = this.__$window.height() - (this.__$container.offset().top - this.__$window.scrollTop());
+        return windowHeight - this.__$container.height() + this.__settings.scrollThreshold + 1 > 0;
+    }
+
+    __intervalCheckScrollPosition() {
+        if (this.__settings.autoTrigger) {
+            setInterval(() => {
+                if (this.__reached()) {
+                    if (!this.__loading && !this.__done && this.__autoTriggerUntilHanddler()) {
+                        this.__reachedStatus = true;
+                        if (this.__settings.cache) {
+                            this.__callbacks['reached'](this.urlConfig, this.__getInfo());
+                        } else {
+                            this.__loading = true;
+                            this.__ajaxRequest(this.__urlConfig);
+                        }
+                    }
+                }
+            }, this.__settings.intervalTime);
+        }
+    }
+
     __bindEventListeners() {
         if (this.__settings.autoTrigger) {
-            this.__$window.on('scroll', () => {
-                let windowHEight = this.__$window.height() - (this.__$container.offset().top - this.__$window.scrollTop());
-                if (windowHEight - this.__$container.height() - this.__$container.scrollTop() + this.__settings.scrollThreshold + 1 > 0) {
+            this.__$window.on('scroll touchmove', () => {
+                if (this.__reached()) {
                     if (!this.__loading && !this.__done && this.__autoTriggerUntilHanddler()) {
-                        this.__loading = true;
-                        this.__ajaxRequest();
+                        this.__reachedStatus = true;
+                        if (this.__settings.cache) {
+                            this.__callbacks['reached'](this.urlConfig, this.__getInfo());
+                        } else {
+                            this.__loading = true;
+                            this.__ajaxRequest(this.__urlConfig);
+                        }
                     }
                 }
             })
@@ -275,8 +386,12 @@ class InfiniteScroll {
         if (this.__settings.nextSelector) {
             this.__$container.on('click touch', this.__settings.nextSelector, () => {
                 if (!this.__loading && !this.__done && this.__autoTriggerUntilHanddler()) {
-                    this.__loading = true;
-                    this.__ajaxRequest();
+                    if (this.__settings.cache) {
+                        this.__callbacks['reached'](this.urlConfig, this.__getInfo());
+                    } else {
+                        this.__loading = true;
+                        this.__ajaxRequest(this.__urlConfig);
+                    }
                 }
             });
         }
